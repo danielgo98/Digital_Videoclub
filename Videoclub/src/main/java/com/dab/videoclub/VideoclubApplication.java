99919e26fb0318e72ea00ed73efceacfd815dfa1
danielgo98 <daniel.andres1998@gmail.com>
2023-12-19 18:33:27 +0100

    feat: add SpringBoot Java project

---
+++ Videoclub/src/main/java/com/dab/videoclub/VideoclubApplication.java
@@ -0,0 +1,14 @@
+package com.dab.videoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoclubApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ Videoclub/src/test/java/com/dab/videoclub/VideoclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dab.videoclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
