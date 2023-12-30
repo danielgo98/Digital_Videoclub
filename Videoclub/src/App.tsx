52f3b5468eca9aa5de08eb08305a8a98736c902a
Imcasero <diegocaserosmr@gmail.com>
2023-12-30 13:20:59 +0100

    feat: add basic model for frontend

---
+++ Videoclub/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-700 flex justify-center items-center min-h-[100vh]">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
