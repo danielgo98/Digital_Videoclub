3b62f8ad32d4b476275dc6e0e996f1bb6d696b4f
danielgo98 <daniel.andres1998@gmail.com>
2024-07-01 21:33:47 +0200

    feat: created auth service

--- Frontend/Videoclub/src/app/auth/services/auth.service.ts
+++ Frontend/Videoclub/src/app/auth/services/auth.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-  constructor() { }
+
+  private baseUrl: string = 'http://localhost:8080';
+
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  login(user: User): Observable<User> {
+    return this.http.post<User>(this.baseUrl + '/user' + '/login', user);
+  }
+
+  register(user: User): Observable<User> {
+    return this.http.post<User>(this.baseUrl + '/user', user);
+  }
 
 }
